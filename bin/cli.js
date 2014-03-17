83aa5801d8b38a974bd62769d66bf2cf369b46c9
Alan Bradley <alan@gangleri.net>
2014-03-17 23:48:07 +0000

    updates from testing

--- bin/cli.js
+++ bin/cli.js
@@ -10,22 +10,26 @@ childProc.exec('ls',{cwd: './node_modules'}, function(err, stdout) {
     process.exit(1);
   }
 
-  var folders = stdout.split('\n');
-  var pkg = require('./package.json');
-  var dependencies = pkg.dependencies.concat(pkg.devDependencies);
+  var folders = stdout.toString().split('\n');
+  var pkg = require('../../../package.json');
+  var dependencies = (Object.keys(pkg.dependencies || {})).concat((Object.keys(pkg.devDependencies || {})));
   var missing = [];
 
-  for(var i =0; i < folders.length; i++) {
-    if(dependencies.indexof(folders[i])) {
+  for(var i=0; i < folders.length; i++) {
+    if(folders[i] !== '' && dependencies.indexOf(folders[i]) < 0) {
       missing.push(folders[i]);
     }
   }
 
   if(missing.length > 0) {
     console.log('The follwing modules have not been added to your package.json');
+    for(var x=0; i < missing.length; x++) {
+      console.log('- ' + missing[x]);
+    }
     process.exit(1);
   } else {
     process.exit(0);
   }
 
 });
+
