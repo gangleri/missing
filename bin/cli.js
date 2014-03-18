23ff2080cf814a84f0822e2ff4fb4fa6cbd2a4f7
Alan Bradley <alan@gangleri.net>
2014-03-18 09:38:27 +0000

    testing changes

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var childProc = require('child_process');
+var path = require('path');
 
 childProc.exec('ls',{cwd: './node_modules'}, function(err, stdout) {
   if(err) {
@@ -10,20 +11,20 @@ childProc.exec('ls',{cwd: './node_modules'}, function(err, stdout) {
     process.exit(1);
   }
 
-  var folders = stdout.toString().split('\n');
-  var pkg = require('../../../package.json');
+  var folders = stdout.toString().trim().split('\n');
+  var pkg = require(path.join(__dirname, '../../../', 'package.json'));
   var dependencies = (Object.keys(pkg.dependencies || {})).concat((Object.keys(pkg.devDependencies || {})));
   var missing = [];
 
   for(var i=0; i < folders.length; i++) {
-    if(folders[i] !== '' && dependencies.indexOf(folders[i]) < 0) {
+    if(dependencies.indexOf(folders[i]) < 0) {
       missing.push(folders[i]);
     }
   }
 
   if(missing.length > 0) {
     console.log('The follwing modules have not been added to your package.json');
-    for(var x=0; i < missing.length; x++) {
+    for(var x=0; x < missing.length; x++) {
       console.log('- ' + missing[x]);
     }
     process.exit(1);
@@ -32,4 +33,5 @@ childProc.exec('ls',{cwd: './node_modules'}, function(err, stdout) {
   }
 
 });
+
 
